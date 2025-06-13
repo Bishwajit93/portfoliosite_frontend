d3207393f0914aa486724fa6d5a736367cabe1f4
bishwajit93 <bish.karm123@gmail.com>
2025-06-14 00:04:59 +0200

    Add layout and all main pages: about, projects, experience, education, contact

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -7,10 +7,10 @@ export default function EducationPage() {
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">🎓 Education</h1>
         <p className="text-gray-600">
-          I’m pursuing a Master's degree in Scientific Computing at TU Berlin, with focus on numerical mathematics, machine intelligence, and optimization.
+          I am  pursuing a Master&apos;s degree in Scientific Computing at TU Berlin, with focus on numerical mathematics, machine intelligence, and optimization.
         </p>
         <p className="text-gray-600 mt-2">
-          Before this, I completed my Bachelor’s degree in Bangladesh with a strong foundation in mathematics and logic, which continues to support my programming journey today.
+          Before this, I completed my Bachelor&apos;s degree in Bangladesh with a strong foundation in mathematics and logic, which continues to support my programming journey today.
         </p>
       </div>
     </Layout>
